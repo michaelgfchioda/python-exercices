e64e143dccaa6a5e9357a2d2e60a78b074d7a156
Michael Chioda <michaelgfchioda14@gmail.com>
2025-09-21 09:05:44 -0300

    Criando diretório do Curso Em Video

---
+++ Curso-Em-Video/mundo_1/aulas/aula06a.py
@@ -0,0 +1,6 @@
+print('Digite dois números para soma: ')
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+soma = num1 + num2
+# print('A soma entre', num1, 'e', num2, 'é: ', soma)
+print('A soma entre {} e {} é {}'.format(num1, num2, soma))
